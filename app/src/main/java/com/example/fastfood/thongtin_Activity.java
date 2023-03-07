76c96c0965d780d4ffd75d6df0a15b17fcc425c1
HHop02 <vihoanghop2604@gmail.com>
2023-03-07 23:06:28 +0700

    Code trang Tôi

---
+++ app/src/main/java/com/example/fastfood/thongtin_Activity.java
@@ -0,0 +1,10 @@
+package com.example.fastfood;
+
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+public class thongtin_Activity  {
+    LinearLayout l1,l2;
+    Button dangxuat;
+
+}
